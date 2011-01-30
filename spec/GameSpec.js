e8c170367f7f91b2c7ea04e9b028e0ef89267dbe
Eirik <eirikb@moss.(none)>
2011-01-30 18:58:17 +0100

    Game has now much better handling for explosions. Also with fires

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -25,7 +25,7 @@ describe("Game", function() {
 
 		g.bricks = [
 		new Box(16, 0, 16, 16), new Box(48, 32, 16, 16), new Box(32, 48, 16, 16), new Box(16, 64, 16, 16)];
-        g.bricks[0].armor = 2;
+		g.bricks[0].armor = 2;
 
 		_.each(g.bricks, function(brick) {
 			g.addBody(brick);
@@ -54,18 +54,89 @@ describe("Game", function() {
 		g.addBody(b7);
 		g.addBody(b8);
 
-        expect(g.getBomb(32, 32)).toBe(b5);
+		expect(g.getBomb(32, 32)).toBe(b5);
 		expect(g.players.length).toBe(4);
 		expect(g.bricks.length).toBe(4);
 		var data = g.explodeBomb(b1);
-        console.log('data', data);
-		expect(data.bodies.length).toBe(14);
+		expect(data.bodies.length).toBe(6);
+		g.removeBodies(data.bodies, Box);
+		g.removeBodies(data.bodies, Player);
 		expect(g.players.length).toBe(1);
-		g.removeBoxes(b1, data);
 		expect(g.bricks.length).toBe(1);
 
-        expect(g.getBomb(32, 32)).toBe(null);
+		expect(g.getBomb(32, 32)).toBe(null);
+	});
 
+});
+describe("Game - general bomb testing", function() {
+	it("should block fires if it is a box", function() {
+		var g = new Game(100, 100),
+		addBomb = function(x, y, power, size) {
+			var bomb = new Bomb(x, y, 10, 10);
+			bomb.power = power;
+			bomb.size = size;
+			g.addBody(bomb);
+			return bomb;
+		},
+		addBox = function(x, y, armor) {
+			var box = new Box(x, y, 10, 10);
+			box.armor = armor;
+			g.addBody(box);
+			return box;
+		},
+		b1 = addBox(20, 10, 1),
+		b2 = addBox(10, 20, 2),
+		b3 = addBox(30, 20, 3),
+		b4 = addBox(20, 30, 4),
+		b = addBomb(20, 20, 1, 1);
+		var data = g.explodeBomb(b);
+		expect(data.bodies.length).toBe(1);
+        expect(data.fires.length).toBe(1);
+        expect(g.world.getBodies(b1)).toContain(b1);
+
+        b.size = 2;
+        data = g.explodeBomb(b);
+        expect(data.bodies.length).toBe(1);
+        expect(data.fires.length).toBe(1);
+        expect(g.world.getBodies(b1)).toContain(b1);
+
+        g.removeBodies(data.bodies, Box);
+        expect(g.world.getBodies(b1)).not.toContain(b1);
+
+        data = g.explodeBomb(b);
+        expect(data.bodies.length).toBe(0);
+        expect(data.fires.length).toBe(3);
+
+        addBomb(20, 0, 1, 2);
+        addBomb(40, 0, 1, 2);
+        addBomb(40, 20, 10, 1);
+        data = g.explodeBomb(b);
+        console.log(data)
+        expect(data.bodies.length).toBe(1);
+        expect(data.fires.length).toBe(13);
+        expect(g.world.getBodies(b4)).toContain(b4);
+        g.removeBodies(data.bodies, Box);
+        expect(g.world.getBodies(b4)).toContain(b4);
+
+        var gf = function(data, x, y) {
+            return _.detect(data.fires, function(fire) {
+                return fire.x === x && fire.y === y;
+            });
+        };
+
+        console.log(gf(data, 20, 0))
+        expect(g.world.getBodies(b4)).toContain(b4);
+        expect(gf(data, 20, 20).firevar).toBe('c');
+        expect(gf(data, 20, 10).firevar).toBe('v');
+        expect(gf(data, 20, 0).firevar).toBe('c');
+        expect(gf(data, 10, 0).firevar).toBe('h');
+        expect(gf(data, 30, 0).firevar).toBe('h');
+        expect(gf(data, 40, 0).firevar).toBe('c');
+        expect(gf(data, 50, 0).firevar).toBe('h');
+        expect(gf(data, 40, 10).firevar).toBe('v');
+        expect(gf(data, 40, 20).firevar).toBe('c');
+        expect(gf(data, 50, 20).firevar).toBe('r');
+        expect(gf(data, 40, 30).firevar).toBe('d');
 	});
 });
 
